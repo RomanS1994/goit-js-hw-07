c61e94836ed9dea507e0e8d932b5fc10efb4f8b0
Roman Stryzhka <rstrizhko1994@gmail.com>
2025-05-05 10:07:41 +0200

    fix task 3

--- js/task-3.js
+++ js/task-3.js
@@ -4,5 +4,7 @@ const nameOutput = document.querySelector("#name-output");
 input.addEventListener("input", () => {
   if (input.value.trim()) {
     nameOutput.textContent = input.value.trim();
+  } else {
+    nameOutput.textContent = "Anonymous";
   }
 });
